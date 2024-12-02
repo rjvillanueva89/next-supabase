421c65eefaffec26d83270ccddb85718d047edf0
RJ Villanueva <rj.villanueva89@gmail.com>
2024-12-02 18:37:45 +0800

    added drizzle, added initial schema

---
+++ src/database/schema/todos.ts
@@ -0,0 +1,17 @@
+import { InferInsertModel, InferSelectModel, sql } from 'drizzle-orm'
+import { boolean, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+
+export const todos = pgTable('todos', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  title: text('title').notNull(),
+  completed: boolean('completed').default(false).notNull(),
+  createdAt: timestamp('created_at', { mode: 'string' }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'string' }).$onUpdate(
+    () => sql`now()`
+  ),
+})
+
+export type Todo = InferSelectModel<typeof todos>
+export type InsertTodo = InferInsertModel<typeof todos>
